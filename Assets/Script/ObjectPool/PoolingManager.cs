3e28d7a7c543ca38c519bc1c1cc4ab25874313b6
JaesunHan <kapoochino93@gmail.com>
2020-12-14 18:09:56 +0900

    풀링 프레임 워크 구현중

---
+++ Assets/Script/ObjectPool/PoolingManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PoolingManager : MonoSingleton<PoolingManager>
+{
+
+    protected Queue<GameObject> _listUnUsed = new Queue<GameObject>();
+    protected Queue<GameObject> _listUsed = new Queue<GameObject>();
+
+    private GameObject pTargetObject = null;
+
+
+    public void DoPrepare_Pool(GameObject pObejctCopyTarget, int iCount)
+    {
+        
+
+        pTargetObject = pObejctCopyTarget;
+
+        if (_listUnUsed.Count <= 0)
+        {
+            for (int i = 0; i < iCount; ++i)
+            {
+                OnCreate_WhenEmptyPool(pTargetObject);
+            }
+        }
+    }
+
+    public GameObject DoPop()
+    {
+        GameObject pObj_Copy = null;
+
+        if (null == pTargetObject)
+        {
+            DebugLogManager.Log("복사하려는 게임 오브젝트 타겟이 NULL 이다.");
+            return null;
+        }
+       
+        if (_listUnUsed.Count <= 0)
+        {
+            pObj_Copy = OnCreate_WhenEmptyPool(pTargetObject);
+        }
+        else
+        {
+            pObj_Copy = _listUnUsed.Dequeue();
+            _listUsed.Enqueue(pObj_Copy);
+        }
+
+        return pObj_Copy;
+    }
+
+    private GameObject OnCreate_WhenEmptyPool(GameObject pObjectCopyTarget)
+    {
+        GameObject obj_UnUsed = null;
+
+        obj_UnUsed = GameObject.Instantiate(pObjectCopyTarget);
+        obj_UnUsed.name = string.Format("{0}_{1}", pObjectCopyTarget.name, _listUnUsed.Count + _listUsed.Count);
+        obj_UnUsed.transform.SetParent(transform);
+
+        _listUnUsed.Enqueue(obj_UnUsed);
+
+        return obj_UnUsed;
+    }
+}
---
+++ Assets/Script/SphereManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereManager : MonoBehaviour
+{
+    public GameObject pOriginal_Obj = null;
+
+    private PoolingManager _pPool_Sphere = PoolingManager.instance;
+
+    private List<GameObject> _listGameObject = new List<GameObject>();
+
+    private void Start()
+    {
+        _pPool_Sphere.DoPrepare_Pool(pOriginal_Obj, 30);
+
+        StartCoroutine(nameof(OnCoroutine_CreateSphere));
+    }
+
+
+    private IEnumerator OnCoroutine_CreateSphere()
+    {
+        while (true)
+        {
+            var pSphere =  _pPool_Sphere.DoPop();
+            pSphere.transform.SetParent(transform);
+            pSphere.transform.position = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(0, -14));
+            pSphere.transform.localScale = Vector3.one * Random.Range(0.7f, 2.2f);
+
+            _listGameObject.Add(pSphere);
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
