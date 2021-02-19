90fec3836c73524631ff6c1158033e0c5ace16fe
JaesunHan <kapoochino93@gmail.com>
2021-02-19 16:37:34 +0900

    오브젝트 풀 기능 구현

---
+++ Assets/Script/CubePoolTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePoolTest : MonoBehaviour
+{
+    PoolingManager_Component<CubeObject> _pCubeObjectPool = new PoolingManager_Component<CubeObject>();
+    public CubeObject _pOriginal_CubeObj = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pCubeObjectPool.DoInit_Pool(_pOriginal_CubeObj);
+
+        StartCoroutine(nameof(OnCoroutine_CreateCUbe));
+    }
+
+    private IEnumerator OnCoroutine_CreateCUbe()
+    {
+        while (true)
+        {
+            var pNewObj = _pCubeObjectPool.DoPop();
+
+            pNewObj.transform.SetParent(transform);
+            pNewObj.transform.localPosition = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
---
+++ Assets/Script/ObjectPool/PoolingManager_Component.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingManager_Component<CLASS_POOL_TARGER> : MonoSingleton<PoolingManager_Component<CLASS_POOL_TARGER>> 
+    where CLASS_POOL_TARGER : Component
+{
+    const int const_iPool_Default_Count = 30;
+
+    private Queue<GameObject> _qPool = new Queue<GameObject>();
+
+    private GameObject _objRoot = null;
+
+    public void DoInit_Pool(CLASS_POOL_TARGER pOriginalObjectTarget)
+    {
+        _objRoot = new GameObject();
+        
+        Prepare_Objec_Pool(pOriginalObjectTarget);
+
+        _objRoot.name = $"{pOriginalObjectTarget.name} 풀 생성";
+    }
+
+    public Component DoPop()
+    {
+        var pDequeueObj = _qPool.Dequeue();
+        pDequeueObj.hideFlags = HideFlags.None;
+        
+
+        return pDequeueObj.GetComponent<CLASS_POOL_TARGER>();
+    }
+
+    public void DoPush(Component pObject)
+    {
+        _qPool.Enqueue(pObject.gameObject);
+        pObject.hideFlags = HideFlags.HideInHierarchy;
+        pObject.transform.SetParent(_objRoot.transform);
+    }
+
+    /// <summary>
+    /// 오브젝트 풀 준비
+    /// </summary>
+    private void Prepare_Objec_Pool(CLASS_POOL_TARGER pObjectOriginal)
+    {
+        for (int i = 0; i < const_iPool_Default_Count; ++i)
+        {
+            var pObj = Instantiate(pObjectOriginal.gameObject);
+            pObj.hideFlags = HideFlags.HideInHierarchy;
+            pObj.transform.SetParent(_objRoot.transform);
+
+            _qPool.Enqueue(pObj);
+        }
+    }
+}
