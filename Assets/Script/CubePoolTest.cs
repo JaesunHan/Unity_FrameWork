1c7de380058476ddefc8738099ed3a79a7d8815d
JaesunHan <kapoochino93@gmail.com>
2021-02-19 17:37:43 +0900

    DoPop 함수 수정
    풀에서 오브젝트 꺼낼 때, 큐에 오브젝트가 없으면 새로 생성해서 반환하도록 수정

--- Assets/Script/CubePoolTest.cs
+++ Assets/Script/CubePoolTest.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class CubePoolTest : MonoBehaviour
 {
     PoolingManager_Component<CubeObject> _pCubeObjectPool = new PoolingManager_Component<CubeObject>();
+    //PoolingManager_Component<CubeObject> _pCubeObjectPool = PoolingManager_Component<CubeObject>.instance;
+
     public CubeObject _pOriginal_CubeObj = null;
     // Start is called before the first frame update
     void Start()
     {
+        //if (null == _pCubeObjectPool)
+        //    return;
+        _pOriginal_CubeObj.gameObject.SetActive(false);
         _pCubeObjectPool.DoInit_Pool(_pOriginal_CubeObj);
 
         StartCoroutine(nameof(OnCoroutine_CreateCUbe));
@@ -18,11 +23,11 @@ public class CubePoolTest : MonoBehaviour
     {
         while (true)
         {
-            var pNewObj = _pCubeObjectPool.DoPop();
+            var pNewObj = _pCubeObjectPool.DoPop(_pOriginal_CubeObj);
 
             pNewObj.transform.SetParent(transform);
             pNewObj.transform.localPosition = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
-
+            pNewObj.gameObject.SetActive(true);
             yield return new WaitForSeconds(2f);
         }
     }
--- Assets/Script/ObjectPool/PoolingManager_Component.cs
+++ Assets/Script/ObjectPool/PoolingManager_Component.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PoolingManager_Component<CLASS_POOL_TARGER> : MonoSingleton<PoolingManager_Component<CLASS_POOL_TARGER>> 
     where CLASS_POOL_TARGER : Component
 {
-    const int const_iPool_Default_Count = 30;
+    const int const_iPool_Default_Count = 5;
 
     private Queue<GameObject> _qPool = new Queue<GameObject>();
 
     private GameObject _objRoot = null;
+
 
     public void DoInit_Pool(CLASS_POOL_TARGER pOriginalObjectTarget)
     {
@@ -20,16 +21,26 @@ public class PoolingManager_Component<CLASS_POOL_TARGER> : MonoSingleton<Pooling
         _objRoot.name = $"{pOriginalObjectTarget.name} 풀 생성";
     }
 
-    public Component DoPop()
+    public Component DoPop(CLASS_POOL_TARGER pObjectOriginal)
     {
-        var pDequeueObj = _qPool.Dequeue();
+        GameObject pDequeueObj = null;
+        if (_qPool.Count <= 0)
+        {
+            var pObj = Instantiate(pObjectOriginal.gameObject);
+            pObj.hideFlags = HideFlags.HideInHierarchy;
+            pObj.transform.SetParent(_objRoot.transform);
+
+            _qPool.Enqueue(pObj);
+        }
+        
+        pDequeueObj = _qPool.Dequeue();
         pDequeueObj.hideFlags = HideFlags.None;
         
 
         return pDequeueObj.GetComponent<CLASS_POOL_TARGER>();
     }
 
-    public void DoPush(Component pObject)
+    public void DoPush(CLASS_POOL_TARGER pObject)
     {
         _qPool.Enqueue(pObject.gameObject);
         pObject.hideFlags = HideFlags.HideInHierarchy;
